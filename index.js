52e3f4bcece7ddfde8e0631ad9f4a723606fa083
Penelope Drago <prd@drago.com.au>
2023-10-04 21:23:03 +1100

    add static methods

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1)
+  }
+
+  static sanitize(text) {
+    return text.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+
+  static titleize(text) {
+    let words = text.split(" ");
+    let newWords = [];
+    let lowerCaseWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    for (let index in words) {
+      if (index == 0 || !lowerCaseWords.includes(words[index])) {
+        newWords.push(Formatter.capitalize(words[index]));
+      } else {
+        newWords.push(words[index]);
+      }
+    }
+    return newWords.join(" ");
+
+  }
+
 }
